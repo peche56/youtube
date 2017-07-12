def9d39ffe5e197687193dcfde7ef93cd0dc2291
peche56 <c.filleul56@gmail.com>
2017-07-12 13:23:38 +0200

    trying to get youtubes datas

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
 import './index.css';
+const API_KEY = 'AIzaSyCngGEeH0jbC0tMJSmgY7Fygo08nBmyB-g';
 
+YTSearch({key: API_KEY, term: 'surf'}, function(data){
+console.log(data);
+})
+
+const App =() => {
+  return (
+    <div>
+      <SearchBar />,
+    </div>
+  );
+}
 ReactDOM.render(
-  <SearchBar />,
+  <App />,
   document.getElementById('root')
 );
